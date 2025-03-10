b27c606eaad6c958583935a2483dac2c932b8ef9
Urwashi97 <urwashi.thakre97@gmail.com>
2025-03-10 19:59:16 +0530

    show hide chart/table

--- client/src/app/components/statistics/statistics.component.ts
+++ client/src/app/components/statistics/statistics.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -10,6 +10,14 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatTableModule } from '@angular/material/table';
 import { StatisticsService } from '../../services/statistics.service';
 import { Chart } from 'chart.js/auto';
+
+export interface StatisticData {
+  weekday: string;
+  reservation_count: number;
+  total_people: number;
+  avg_group_size: number;
+  weekdayName: string;
+}
 
 @Component({
   selector: 'app-statistics',
@@ -29,13 +37,31 @@ import { Chart } from 'chart.js/auto';
   styleUrl: './statistics.component.scss',
 })
 export class StatisticsComponent implements OnInit {
+  @ViewChild('chartCanvas') chartCanvas!: ElementRef<HTMLCanvasElement>;
+
+  statisticsData: StatisticData[] = [];
+  showTable: boolean = false;
+  displayedColumns: string[] = [
+    'weekdayName',
+    'reservation_count',
+    'total_people',
+    'avg_group_size',
+  ];
   statisticForm = new FormGroup({
     betriebId: new FormControl(201216, { nonNullable: true }),
     startDate: new FormControl(new Date('2023-10-01')),
     endDate: new FormControl(new Date('2023-12-31')),
   });
-
-  private chartInstance: Chart | null = null;
+  weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  private chartInstance: Chart | undefined;
 
   constructor(private statisticsService: StatisticsService) {}
 
@@ -60,62 +86,66 @@ export class StatisticsComponent implements OnInit {
 
     this.statisticsService
       .getStatistics(validBetriebId, formattedStartDate, formattedEndDate)
-      .subscribe((data) => {
-        console.log(`Fetched Data`, data);
-        this.renderChart(data);
-      });
+      .subscribe(
+        (data) => {
+          console.log(`Fetched Data`, data);
+          this.statisticsData = Array.isArray(data)
+            ? data.map((item) => ({
+                ...item,
+                weekdayName: this.weekdays[parseInt(item.weekday, 10)],
+              }))
+            : [];
+          this.renderChart(data);
+        },
+        (error) => {
+          console.error('Error fetching statistics:', error);
+        }
+      );
+  }
+
+  toggleTable() {
+    this.showTable = !this.showTable;
+    console.log(this.showTable);
+    if (!this.showTable) {
+      setTimeout(() => this.renderChart(this.statisticsData), 100);
+    }
   }
 
   renderChart(data: any[]) {
-    const canvas = document.getElementById(
-      'reservationChart'
-    ) as HTMLCanvasElement;
-
-    if (Chart.getChart(canvas)) {
-      Chart.getChart(canvas)?.destroy();
-    }
+    const canvas = this.chartCanvas?.nativeElement?.getContext('2d');
 
     if (!data || data.length === 0) {
       console.error(`No data available for chart rendering`);
       return;
     }
 
-    const label = data.map(
-      (day) =>
-        [
-          'Sunday',
-          'Monday',
-          'Tuesday',
-          'Wednesday',
-          'Thursday',
-          'Friday',
-          'Saturday',
-        ][Number(day.weekday)]
-    );
+    const label = data.map((day) => this.weekdays[Number(day.weekday)]);
 
     const reservations = data.map((d) => d.reservation_count);
 
-    this.chartInstance = new Chart(canvas, {
-      type: 'bar',
-      data: {
-        labels: label,
-        datasets: [
-          {
-            label: 'Reservations',
-            data: reservations,
-            backgroundColor: 'rgba(75,192,192,0.2)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        scales: {
-          y: { beginAtZero: true },
+    if (canvas) {
+      this.chartInstance = new Chart(canvas, {
+        type: 'bar',
+        data: {
+          labels: label,
+          datasets: [
+            {
+              label: 'Reservations',
+              data: reservations,
+              backgroundColor: 'rgba(75,192,192,0.2)',
+              borderColor: 'rgba(75,192,192,1)',
+              borderWidth: 1,
+            },
+          ],
         },
-      },
-    });
+        options: {
+          responsive: true,
+          scales: {
+            y: { beginAtZero: true },
+          },
+        },
+      });
+    }
   }
 
   ngOnDestry() {
